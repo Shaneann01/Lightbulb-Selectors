75ddf3e5dd6e6ddba08ca6083b7cc4f05c528242
Shaneann01 <shaneann.ahtack@vodafone.com>
2023-10-19 15:50:50 +0100

    finished exercise

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // Write your code here
+console.log("done");
+const bulb1 = document.querySelector("#lightbulb1");
+const bulb2 = document.querySelector("#lightbulb2");
+const bulb3 = document.querySelector("#lightbulb3");
+
+const output = document.querySelector(".subtitle");
+
+let count = 0;
+
+bulb1.addEventListener("click", function () {
+  count += 1;
+  bulb1.classList.toggle("active");
+  output.innerHTML = `You've clicked the lights ${count} times`;
+});
+
+bulb2.addEventListener("click", function () {
+  count += 1;
+  bulb2.classList.toggle("active");
+  output.innerHTML = `You've clicked the lights ${count} times`;
+});
+
+bulb3.addEventListener("click", function () {
+  count += 1;
+  bulb3.classList.toggle("active");
+  output.innerHTML = `You've clicked the lights ${count} times`;
+});
